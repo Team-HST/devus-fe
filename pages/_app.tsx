29dcdb23bc052e19604b045d7c0e57a96a0cca4c
dudgns0612 <dudgns0612@naver.com>
2022-03-28 00:28:11 +0900

    mobile check example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,36 @@
+import { useEffect, useState } from 'react';
+import type { AppProps, AppContext } from 'next/app';
+import type { NextPageContext } from 'next';
 import '../styles/globals.scss';
-import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+interface AppInitProps extends AppProps {
+  isSSRMobile: boolean;
+  mode: string;
 }
 
-export default MyApp;
+function DevUs({ Component, pageProps, isSSRMobile, mode }: AppInitProps) {
+  const [isMobile, setIsMobile] = useState(isSSRMobile);
+
+  useEffect(() => {
+    if (mode === 'CSR') {
+      setIsMobile(navigator.userAgent.indexOf('Mobi') > -1);
+    }
+  }, [mode]);
+
+  return <Component {...{ ...pageProps, isMobile }} />;
+}
+
+DevUs.getInitialProps = async ({ ctx: { req } }: AppContext) => {
+  let isSSRMobile = false;
+  let mode = 'SSR';
+  // mobile 유무 체크
+  if (req && req.headers['user-agent']) {
+    isSSRMobile = req.headers['user-agent'].indexOf('Mobi') > -1;
+  } else {
+    mode = 'CSR';
+  }
+
+  return { isSSRMobile, mode };
+};
+
+export default DevUs;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
-import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
-const Home: NextPage = () => {
+interface HomeProps {
+  isMobile: boolean;
+}
+
+const Home = ({ isMobile }: HomeProps) => {
   return (
     <div className={styles.container}>
       <Head>
